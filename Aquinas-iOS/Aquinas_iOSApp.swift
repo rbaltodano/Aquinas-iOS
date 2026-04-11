9364c98ec2236211d421a156016b5fe4e9b27686
Ryan Baltodano <rbaltodano337@gmail.com>
2026-04-11 12:16:28 -0400

    Initial Commit

---
+++ Aquinas-iOS/Aquinas_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aquinas_iOSApp.swift
+//  Aquinas-iOS
+//
+//  Created by Ryan on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Aquinas_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
